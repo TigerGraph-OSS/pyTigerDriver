88dc26c7faaf4e35a9f9e584203c4e0277f0bfd4
Zrouga Mohamed <zrouga.mohammed@gmail.com>
2021-02-04 11:04:05 +0100

    Upgrade ...

--- pyTigerDriver/__init__.py
+++ pyTigerDriver/__init__.py
@@ -8,6 +8,7 @@ class Client():
     def __init__(self, server_ip="127.0.0.1", username="tigergraph", password="tigergraph", cacert=""
                  ,version="3.0.5", commit="",restPort="9000",gsPort="14240",protocol="https"
                  ,graph="MyGraph",token=""):
-        self.Rest = REST_Client(server_ip=server_ip,protocol=protocol,restPort=restPort,token=token)
+        self.Rest = REST_Client(server_ip=server_ip,protocol=protocol,restPort=restPort,token=token
+                                , username=username, password=password, cacert=cacert)
         self.Gsql = GSQL_Client(server_ip=server_ip, username=username, password=password, cacert=cacert
                            ,version=version, commit=commit,graph=graph,gsPort=gsPort,protocol=protocol)
